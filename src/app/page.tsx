bd26fd363f8510361d1adfc8492c99c416855ff1
AbhishekJamdade <jamdadeabhishek039@gmail.com>
2025-09-11 23:53:02 +0530

    Bookmark form Implmented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,98 @@
-import Image from "next/image";
+'use client'
+
+import {
+  SignedIn , SignedOut
+} from '@clerk/nextjs'
+import { useBookmarks } from '@/hooks/useBookmarks';
+import { CreateBookmarkData } from '@/types/bookmark';
+import { useState } from 'react';
 import styles from "./page.module.css";
+import BookmarkForm from '@/components/BookmarkForm';
 
 export default function Home() {
+  const {
+    bookmarks,
+    loading,
+    error,
+    addBookmark,
+    deleteBookmark,
+    refetch
+} = useBookmarks()
+
+const [showAddForm, setShowAddForm] = useState(false)
+const [ isSubmitting, setIsSubmitting] = useState(false)
+
+const handleAddBookmark = async(data:CreateBookmarkData) =>{
+  try {
+    setIsSubmitting(true)
+    await addBookmark(data)
+    setShowAddForm(false)
+  } catch(e){
+    console.log(e)
+  } finally{
+    setIsSubmitting(false )
+  }
+}
+
+const handleDeleteBookmark = async(id : string) =>{
+  if(confirm("Are you sure you want to delete?"))
+  try {
+    await deleteBookmark(id)
+    setShowAddForm(false)
+  } catch(e){
+    console.log("Failed to delete the bookmark", e)
+  }
+}
+
+if(loading){
+  return <div className={styles['loading-container']}>
+    <div  className={styles['loading-content']}>
+      <div  className={styles['loading-spinner']}>
+        <p>loading bookmarks....</p>
+      </div>
+    </div>
+  </div>
+}
+
   return (
-    <div className={styles.page}>
-      
+    <div>
+      <SignedIn>
+      <div className='container'>
+          <div className={styles['page-header']}>
+            <div className={styles['header-content']}>
+              <div className={styles['header-text']}>
+                  <h1>Bookmark Manager</h1>
+                  <p>Organise your Bookmarks</p>
+              </div>
+              <div className={styles['header-button']}>
+                <button className='btn btn-primary' onClick={()=> setShowAddForm(!showAddForm)}>
+                  {showAddForm ? "Cancel" : "Add Bookmark"}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {
+          error && <div className={styles['error-message']}>
+            <p>Error: {error}</p>
+            <button onClick={refetch} className='btn btn-secondary'>
+              Retry
+            </button>
+          </div>
+        }
+        { showAddForm && <div className={styles['form-section ']}>
+          <BookmarkForm onSubmit={handleAddBookmark} isSubmitting={isSubmitting}></BookmarkForm>
+          </div>}
+      </SignedIn>
+      <SignedOut>
+        <div className={styles['signout-container']}>
+          <div className={styles['signout-content']}>
+            <div className={styles['signout-text']}>
+              Please signin to manage your Bookmarks
+            </div>
+          </div>
+        </div>
+      </SignedOut>
     </div>
   );
 }
---
+++ src/components/BookmarkForm.tsx
@@ -0,0 +1,75 @@
+import { CreateBookmarkData } from '@/types/bookmark'
+import { useState } from 'react';
+
+interface BookmarkFormProps{
+    onSubmit: (data: CreateBookmarkData) => void;
+    initialData? : Partial<CreateBookmarkData>;
+    isSubmitting: boolean;
+}
+
+export default function BookmarkForm({onSubmit, initialData, isSubmitting=false}:BookmarkFormProps){
+    const [formData, setFormData] = useState<CreateBookmarkData>({
+        url: initialData?.url || ' ',
+        title: initialData?.title || ' ',
+        notes: initialData?.notes || ' ',
+    })
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        if(!formData.url.trim()|| !formData.title.trim()||isSubmitting){
+            return
+        }
+        onSubmit(formData)
+    }
+
+  return (
+    <>
+    <form onSubmit={handleSubmit} className='form'>
+        <h2 className='form-title'>
+            Add new bookmark
+        </h2>
+        <div className='form-group'>
+            <label htmlFor='url' className='form-label'>URL *</label>
+            <input 
+                type='url'
+                id='url'
+                onChange={(e)=>setFormData((prev)=>({...prev, url: e.target.value}))}
+                className='form-input'
+                placeholder='https://example.com'
+                required
+                disabled={isSubmitting}
+            />
+        </div>
+        <div className='form-group'>
+            <label htmlFor='title' className='form-label'>Title </label>
+            <input 
+                type='title'
+                id='title'
+                onChange={(e)=>setFormData((prev)=>({...prev, title: e.target.value}))}
+                className='form-input'
+                placeholder='Enter title'
+                required
+                disabled={isSubmitting}
+            />
+        </div>
+        <div className='form-group'>
+            <label htmlFor='notes' className='form-label'>Notes </label>
+            <input 
+                type='notes'
+                id='notes'
+                onChange={(e)=>setFormData((prev)=>({...prev, notes: e.target.value}))}
+                className='form-input'
+                placeholder='Enter notes'
+                required
+                disabled={isSubmitting}
+            />
+        </div>
+
+        <button type='submit' className='btn btn-primary' disabled={isSubmitting}>
+            {isSubmitting ? "Saving..." : "Save Bookmark"}
+        </button>
+    </form>
+    </>
+  )
+}
+
+
